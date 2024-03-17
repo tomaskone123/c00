a75f6178f65dac66e64bd46ae39677c3b3690047
Tomas Konecny <tkkrumlovska@seznam.cz>
2024-03-17 22:53:43 +0100

    finished code without the 42 header

--- c06/ex00/ft_print_program_name.c
+++ c06/ex00/ft_print_program_name.c
@@ -1,4 +1,16 @@
- int main(int argc, char *argv[])
- {
-    
- }
+#include <unistd.h>
+#include <stdio.h>
+
+int	main(int argc, char *argv[])
+{
+	char	*temp;
+
+	temp = argv[0];
+	while (*temp)
+	{
+		write(1, temp, 1);
+		temp++;
+	}
+	// printf("%s", temp);
+	return (0);
+}
